0473289e367a431baea3cfe65ef17f24d94225bc
JonasAntonio <jayv.jonas@gmail.com>
2017-01-31 21:36:49 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author jonas
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.println("A média é igual à "+ m);
+        
+        int n = 5;
+        int valor = 5 + ++n;
+        System.out.println(valor);
+       
+        float x =(float) Math.sqrt(25);
+        System.out.println(x);
+        
+        float v = (float) 8.3;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+        int ar1 = (int) Math.ceil(v);
+        System.out.println(ar1);
+        int ar2 = (int) Math.round(v);
+        System.out.println(ar2);
+        */
+        double ale = 5+(Math.random()*(10-5));
+        System.out.println(ale);
+    }
+    
+}
