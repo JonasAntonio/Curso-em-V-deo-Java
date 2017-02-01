3ff681db702d33fede9c0f50ead59d143c8ca11e
JonasAntonio <jayv.jonas@gmail.com>
2017-01-31 21:39:13 -0300

    Add files via upload

---
+++ Repeticao02/src/repeticao02/Repeticao02.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticao02;
+
+/**
+ *
+ * @author james
+ */
+public class Repeticao02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*for( int i = 0; i <10; i++){
+            System.out.println(i);
+        }
+        for( int i = 0; i <10; i+=2){
+            System.out.println(i);
+        }
+        for( int i = 10; i > 0; i--){
+            System.out.println(i);
+        }*/
+        for(int i = 0; i <= 100; i += 10){
+            System.out.println(i);
+        }
+    }
+    
+}
